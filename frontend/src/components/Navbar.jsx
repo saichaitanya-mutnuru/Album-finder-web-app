0b50b0b4480fad7806941ecd22a90ff954011c63
cyb3rdaddy <msaichaitanya10b@gmail.com>
2026-07-05 23:43:12 +0530

    Final commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import logo from "../assets/logo.png";
+
 function Navbar() {
   return (
     <nav className="navbar">
 
       <h1 className="logo">
-        <img src="/logo.png" alt="logo" />
-        Album Finder
+        <img src={logo}  alt="logo " />
+         Album Finder
       </h1>
 
       <div className="nav-right">
